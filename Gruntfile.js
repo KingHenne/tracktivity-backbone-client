f4b0eb3eaec54445409066956a26132fc6dd3771
Hendrik Liebau <hendrik.liebau@sinnerschrader.com>
2013-10-02 09:51:54 +0200

    deactivated unwanted grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -223,7 +223,7 @@ module.exports = function (grunt) {
 					src: [
 						'<%= yeoman.dist %>/scripts/{,*/}*.js',
 						'<%= yeoman.dist %>/styles/{,*/}*.css',
-						'<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
+						//'<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
 						'<%= yeoman.dist %>/styles/fonts/{,*/}*.*'
 					]
 				}
@@ -372,7 +372,7 @@ module.exports = function (grunt) {
 			'concurrent:server',
 			'autoprefixer',
 			'connect:livereload',
-			'open',
+			//'open',
 			'watch'
 		]);
 	});
